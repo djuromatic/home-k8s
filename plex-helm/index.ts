ebab2d09c5c2a21edece0f26d559ea6591ae7e92
djuromatic <djuromatic991@gmail.com>
2023-11-24 15:19:07 +0100

    annotation in igress

--- plex-helm/index.ts
+++ plex-helm/index.ts
@@ -8,26 +8,25 @@ const serviceName = "plex-service"
 const appLabel = "plex"
 
 const ingressNs = new k8s.core.v1.Namespace("plexns", {
-	metadata: {
-	    	name: namespace,
-	}
+  metadata: {
+    name: namespace,
+  }
 });
 
 const ingress = new k8s.networking.v1.Ingress("plex-ingress", {
-  metadata: { 
+  metadata: {
     name: "plex-ingress",
-    namespace, 
+    namespace,
     annotations: {
-            "kubernetes.io/ingress.class": "public",
-	    "cert-manager.io/cluster-issuer": "lets-encrypt"
+      "cert-manager.io/cluster-issuer": "lets-encrypt"
     },
   },
   spec: {
-    tls:[{
+    tls: [{
       hosts: [
         domain
       ],
-    secretName: "plex-tls-secret",
+      secretName: "plex-tls-secret",
     }
     ],
     rules: [
@@ -36,12 +35,12 @@ const ingress = new k8s.networking.v1.Ingress("plex-ingress", {
         http: {
           paths: [
             {
-              path: "/",
+              path: "/web",
               pathType: "Prefix",
               backend: {
                 service: {
                   name: serviceName,
-		  port: { number: 32400 }, // This should be the port where your service is running
+                  port: { number: 32400 }, // This should be the port where your service is running
                 },
               },
             },
@@ -53,32 +52,32 @@ const ingress = new k8s.networking.v1.Ingress("plex-ingress", {
 });
 
 const plexService = new k8s.core.v1.Service("plex-service", {
-    metadata: { name: serviceName, namespace },
-    spec: {
-	type: "NodePort",
-        selector: { app: appLabel }, // Replace with your app label selector
-        ports: [{ port: 32400, targetPort: 32400 }], // Replace with your service port
-    },
+  metadata: { name: serviceName, namespace },
+  spec: {
+    type: "NodePort",
+    selector: { app: appLabel }, // Replace with your app label selector
+    ports: [{ port: 32400, targetPort: 32400 }], // Replace with your service port
+  },
 });
 
 const plexDeployment = new k8s.apps.v1.Deployment("plex-deployment", {
-    metadata: { namespace },
-    spec: {
-        replicas: 1,
-        selector: { matchLabels: { app: appLabel } },
-        template: {
-            metadata: { labels: { app: appLabel } },
-            spec: {
-                containers: [
-                    {
-                        name: appLabel,
-                        image: "linuxserver/plex:latest", // Replace with your Plex image
-                        ports: [{ containerPort: 32400 }], // Replace with your Plex service port
-                    },
-                ],
-            },
-        },
+  metadata: { namespace },
+  spec: {
+    replicas: 1,
+    selector: { matchLabels: { app: appLabel } },
+    template: {
+      metadata: { labels: { app: appLabel } },
+      spec: {
+        containers: [
+          {
+            name: appLabel,
+            image: "linuxserver/plex:latest", // Replace with your Plex image
+            ports: [{ containerPort: 32400 }], // Replace with your Plex service port
+          },
+        ],
+      },
     },
+  },
 });
 
 // Export the Kubernetes cluster kubeconfig
