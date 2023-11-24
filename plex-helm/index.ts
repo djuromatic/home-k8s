7fca6fe7b1276d9c98b598ef877fda22a7621866
djuromatic <djuromatic991@gmail.com>
2023-11-24 16:18:42 +0100

    restore to old version

--- plex-helm/index.ts
+++ plex-helm/index.ts
@@ -1,85 +1,89 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as k8s from "@pulumi/kubernetes";
+import * as kubernetes from "@pulumi/kubernetes";
 
 const config = new pulumi.Config();
-const domain = config.require("domain");
-const namespace = config.require("namespace")
-const serviceName = "plex-service"
-const appLabel = "plex"
+const k8sNamespace = config.require("k8sNamespace");
+const appLabels = {
+  app: "plex",
+};
 
-const ingressNs = new k8s.core.v1.Namespace("plexns", {
+// Create a namespace (user supplies the name of the namespace)
+const ingressNs = new kubernetes.core.v1.Namespace("plexns", {
   metadata: {
-    name: namespace,
+    labels: appLabels,
+    name: k8sNamespace,
   }
 });
 
-const ingress = new k8s.networking.v1.Ingress("plex-ingress", {
+
+const pvc = new kubernetes.core.v1.PersistentVolumeClaim("plex-data-pvc", {
   metadata: {
-    name: "plex-ingress",
-    namespace,
+    name: "plex-data-pvc",
+    namespace: ingressNs.metadata.name,
     annotations: {
-      "cert-manager.io/cluster-issuer": "lets-encrypt"
-    },
+      "pulumi.com/skipAwait": "true" // don't use the await logic at all
+    }
   },
   spec: {
-    tls: [{
+    accessModes: ["ReadWriteOnce"],
+    resources: {
+      requests: {
+        storage: config.require("pvcSize")
+      },
+    },
+  },
+});
+
+// Use Helm to install the Nginx ingress controller
+const ingressController = new kubernetes.helm.v3.Release("plex", {
+  chart: "plex",
+  namespace: ingressNs.metadata.name,
+  repositoryOpts: {
+    repo: "https://utkuozdemir.org/helm-charts",
+  },
+  skipCrds: true,
+  values: {
+    ingress: {
+      enabled: true,
+      annotations: {
+        "cert-manager.io/cluster-issuer": "lets-encrypt"
+      },
       hosts: [
-        domain
-      ],
-      secretName: "plex-tls-secret",
-    }
-    ],
-    rules: [
-      {
-        host: domain,
-        http: {
+        {
+          host: "plex.dmatic.xyz",
           paths: [
             {
               path: "/",
-              pathType: "Prefix",
-              backend: {
-                service: {
-                  name: serviceName,
-                  port: { number: 32400 }, // This should be the port where your service is running
-                },
-              },
-            },
+              pathType: "ImplementationSpecific"
+            }
           ],
-        },
+        }
+      ],
+    },
+    service: {
+      type: "NodePort"
+    },
+    persistence: {
+      data: {
+        enabled: true,
+        existingClaim: pvc.metadata.name,
+        isPvc: true
+      }
+    },
+    controller: {
+      enableCustomResources: false,
+      appprotect: {
+        enable: false,
       },
-    ],
-  },
-});
-
-const plexService = new k8s.core.v1.Service("plex-service", {
-  metadata: { name: serviceName, namespace },
-  spec: {
-    type: "NodePort",
-    selector: { app: appLabel }, // Replace with your app label selector
-    ports: [{ port: 32400, targetPort: 32400 }], // Replace with your service port
-  },
-});
-
-const plexDeployment = new k8s.apps.v1.Deployment("plex-deployment", {
-  metadata: { namespace },
-  spec: {
-    replicas: 1,
-    selector: { matchLabels: { app: appLabel } },
-    template: {
-      metadata: { labels: { app: appLabel } },
-      spec: {
-        containers: [
-          {
-            name: appLabel,
-            image: "linuxserver/plex:latest", // Replace with your Plex image
-            ports: [{ containerPort: 32400 }], // Replace with your Plex service port
-          },
-        ],
+      appprotectdos: {
+        enable: false,
+      },
+      service: {
+        extraLabels: appLabels,
       },
     },
   },
 });
 
-// Export the Kubernetes cluster kubeconfig
-export const kubeconfig = pulumi.output("test");
-
+// Export some values for use elsewhere
+export const name = ingressController.name;
